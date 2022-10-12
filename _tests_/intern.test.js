b97cb93c6dd2b761fb12be24236f65b631eeb04f
SDann <smdann17@gmail.com>
2022-10-12 16:42:15 -0500

    Added tests to intern.test.js to include testing for creation of intern object, getting the intern's school, and the intern's role. Create intern class in intern.js. Ran tests using jest and they passed for intern.

---
+++ _tests_/intern.test.js
@@ -0,0 +1,28 @@
+const Intern = require("../lib/intern");
+
+describe("Intern", () => {
+  describe("name", () => {
+    it("should create an intern object", () => {
+      const intern = new Intern("Sally", 4, "sally@test.com", "U of M");
+
+      expect(intern.school).toEqual("U of M");
+    });
+
+    describe("getSchool", () => {
+      it("should get the intern's school", () => {
+        const intern = new Intern("Sally", 4, "sally@test.com", "U of M");
+  
+        expect(intern.getSchool()).toEqual("U of M");
+      })
+    });
+
+    describe("getRole", () => {
+      it("should get the intern's role", () => {
+        const intern = new Intern("Sally", 4, "sally@test.com", "U of M");
+  
+        expect(intern.getRole()).toEqual("Intern");
+      })
+    });
+
+  });
+})
