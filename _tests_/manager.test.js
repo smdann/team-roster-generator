e875244c9069cf38f62738e77e6d5ce50eaeffe1
SDann <smdann17@gmail.com>
2022-10-12 16:51:36 -0500

    Added tests to manager.test.js file to include tests for creating the manager object and getting the role of the manager via the getRole method. Added manager class to manager.js. Ran tests using jest and all tests passed for manager as well as employee, engineer, and intern.

---
+++ _tests_/manager.test.js
@@ -0,0 +1,20 @@
+const Manager = require("../lib/manager");
+
+describe("Manager", () => {
+  describe("name", () => {
+    it("should create a manager object", () => {
+      const manager = new Manager("Mary", 3, "mary@test.com", 105);
+
+      expect(manager.officeNumber).toEqual(105);
+    });
+
+    describe("getRole", () => {
+      it("should get the manager's role", () => {
+        const manager = new Manager("Mary", 3, "mary@test.com", 105);
+  
+        expect(manager.getRole()).toEqual("Manager");
+      })
+    });
+
+  });
+})
