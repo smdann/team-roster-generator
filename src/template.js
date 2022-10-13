9980a4d3c746fe38de2967a9f3005e31ea10015d
SDann <smdann17@gmail.com>
2022-10-12 20:33:38 -0500

    Using the previously created template.html, created functions to create cards for the manager, engineer, and intern employee types that return string literals containing the information entered by the user.

--- src/template.js
+++ src/template.js
@@ -1,9 +1,53 @@
 // Create the team
 
-    // Create the Manager 
+// Create the manager Card
+const createManager = (manager) => {
+    return `
+    <div class="card employee-card" style="width: 18rem;">
+        <div class="card-header">
+          <h2 class="card-title">${manager.name}</h2>
+          <h3 class="card-title"><i class=""></i>Manager</h3>
+        </div>
+        <ul class="card-body list-group-flush">
+          <li class="list-group-item">ID: ${manager.id}</li>
+          <li class="list-group-item">Email: <a href="mailto:${manager.email}"></a>${manager.email}</li>
+          <li class="list-group-item">Office number: ${manager.officeNumber}</li>
+        </ul>
+      </div>
+    `
+}
 
 
-    // Create the Engineers
+// Create the engineer card(s)
+const createEngineers = (engineer) => {
+    return `
+    <div class="card employee-card" style="width: 18rem;">
+        <div class="card-header">
+          <h2 class="card-title">${engineer.name}</h2>
+          <h3 class="card-title"><i class=""></i>Engineer</h3>
+        </div>
+        <ul class="card-body list-group-flush">
+          <li class="list-group-item">ID: ${engineer.id}</li>
+          <li class="list-group-item">Email: <a href="mailto:${engineer.email}"></a>${engineer.email}</li>
+          <li class="list-group-item">GitHub: ${engineer.github}</li>
+        </ul>
+      </div>
+    `
+}
 
-
-    // Create the Interns
+// Create the intern card(s)
+const createInterns = (intern) => {
+    return `
+    <div class="card employee-card" style="width: 18rem;">
+        <div class="card-header">
+          <h2 class="card-title">${intern.name}</h2>
+          <h3 class="card-title"><i class=""></i>Intern</h3>
+        </div>
+        <ul class="card-body list-group-flush">
+          <li class="list-group-item">ID: ${intern.id}</li>
+          <li class="list-group-item">Email: <a href="mailto:${intern.email}"></a>${intern.email}</li>
+          <li class="list-group-item">School: ${intern.school}</li>
+        </ul>
+      </div>
+    `
+}
