3a2d60c586ec6627a5efdcb7f9a3133f0a9e6e89
SDann <smdann17@gmail.com>
2022-10-12 20:08:26 -0500

    In index.js, added prompts for user to enter team member type and information. Added a function to add employee information to newly created engineers and interns by passing info in as arguments. Created a function to write the file (./dist/index.html) with the employee information content.

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-const Manager = require('./lib/manager');
-const Engineer = require('./lib/engineer');
-const Intern = require('./lib/intern');
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
 
 const teamMembers = [];
 
+const templateHTML = require("./src/template")
+
 // Prompts user for manager information
 const createManager = () => {
+  console.log("Welcome to the Team Roster Generator!")
+  console.log("Use 'npm run reset' to reset the /dist folder.")
+
   return inquirer.prompt([
     {
       type: "input",
@@ -39,7 +44,6 @@ const createManager = () => {
   })
 }
 
-createManager()
 
 // Prompts user for team member type and information
 const createEmployee = () => {
@@ -48,15 +52,79 @@ const createEmployee = () => {
       type: "list",
       message: "Which type of team member would you like to add?",
       name: "role",
-      choices: [Engineer, Intern, "I'm done adding team members."],
+      choices: ["Engineer", "Intern", "I'm done adding team members."],
     },
     {
-
+      type: "input",
+      message: "What is the team member's name?",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "What is the team member's ID?",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "What is the team member's email?",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "What is the team member's GitHub username?",
+      name: "github",
+      when: (list) => list.role === "Engineer",
+    },
+    {
+      type: "input",
+      message: "What school does the team member attend?",
+      name: "school",
+      when: (list) => list.role === "Intern",
+    },
+    {
+      type: "confirm",
+      message: "Would you like to add more team members?",
+      name: "addMoreEmployees",
+      default: false
     }
   ])
+
+  .then(employeeInfo => {
+    let{name, id, email, role, github, school, addMoreEmployees} = employeeInfo;
+    let employee;
+
+    if (role === "Engineer") {
+      employee = new Engineer (name, id, email, github);
+      console.log(employee);
+    } else if (role === "Intern") {
+      employee = new Intern (name, id, email, school);
+      console.log(employee);
+    }
+    teamMembers.push(employee);
+
+    if (addMoreEmployees) {
+      return createEmployee(teamMembers);
+    } else {
+      return teamMembers;
+    }
+  })
 }
 
+const writeFile = htmlFileContent => {
+  fs.writeFile("./dist/index.html", htmlFileContent, err => {
+    err ? console.log(err) : console.log("Your team roster has been generated!")
+  })
+};
 
-
-  // prefix on separate line?
-  // prefix: "Welcome to the Team Roster Generator! Use 'npm run reset' to reset the /dist folder."
+createManager()
+  .then(createEmployee)
+  .then(teamMembers => {
+    return templateHTML(teamMembers);
+  })
+  .then(rosterHTML => {
+    return writeFile(rosterHTML);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+  
